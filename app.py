d64d722b002d0978f48a674976d3d3b592fcd1ec
JC <cai_li@yahoo.com>
2023-12-07 14:28:18 +0800

    add new streamlit for multiple files updaload

--- app.py
+++ app.py
@@ -1,26 +1,64 @@
 import streamlit as st
 import requests
 
-st.title('File Upload to FastAPI')
+st.title('Stroke Prediction')
+uploaded_files = st.sidebar.file_uploader("Upload pairs of files (e.g., nostroke_01.png, stroke_01.png)",
+    type=["png", "jpg"], accept_multiple_files=True)
 
-uploaded_file = st.file_uploader("Choose a file (e.g., a44.png)")
+# Check if files were uploaded and there's an even number for pairs
+if uploaded_files and len(uploaded_files) % 2 == 0:
+    # Iterate over the files in pairs
+    for i in range(0, len(uploaded_files), 2):
+        nostroke_file = uploaded_files[i]
+        stroke_file = uploaded_files[i + 1]
 
-if uploaded_file is not None:
-    # Show details of the file
-    file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
-    st.write(file_details)
+        # Display each pair of images
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.image(nostroke_file, caption='No Stroke', use_column_width=True)
+        with col2:
+            st.image(stroke_file, caption='Stroke', use_column_width=True)
 
-    # url = 'http://localhost:8000/uploadfile/'  # Replace with your FastAPI URL
-    # Send the file to FastAPI server
-    url = 'https://strokediag-xwy6mzfeuq-wn.a.run.app/uploadfile/'
-    files = {'file': (uploaded_file.name, uploaded_file, uploaded_file.type)}
-    response = requests.post(url, files=files)
+        # Send the files to FastAPI server for prediction
+        url = 'https://strokediag-xwy6mzfeuq-wn.a.run.app/uploadfile/'
 
-    # Show the response from the server
-    if response.status_code == 200:
-        st.success('File successfully uploaded and processed.')
-        st.json(response.json())
-    else:
-        st.error('An error occurred during upload.')
-        st.write('Status code:', response.status_code)
-        st.write('Response body:', response.text)
+        # Predict for 'No Stroke'
+        files = {'file': (nostroke_file.name, nostroke_file, nostroke_file.type)}
+        response_nostroke = requests.post(url, files=files)
+
+        # Predict for 'Stroke'
+        files = {'file': (stroke_file.name, stroke_file, stroke_file.type)}
+        response_stroke = requests.post(url, files=files)
+
+        # Show the prediction result
+        with col3:
+            st.write("Predicted Results:")
+            if response_nostroke.status_code == 200 and response_stroke.status_code == 200:
+                # Assuming the response contains the prediction result as {'prediction': 0 or 1}
+                prediction_nostroke = response_nostroke.json().get('prediction')
+                prediction_stroke = response_stroke.json().get('prediction')
+
+                # Determine if the predictions are incorrect
+                nostroke_incorrect = (prediction_nostroke != 0)
+                stroke_incorrect = (prediction_stroke != 1)
+
+                # Highlight incorrect predictions
+                if nostroke_incorrect:
+                    st.markdown(f"<span style='color: red'>**Before Stroke Prediction: Stroke**</span>", unsafe_allow_html=True)
+                else:
+                    st.write("Before Stroke Prediction: No Stroke")
+
+                if stroke_incorrect:
+                    st.markdown(f"<span style='color: red'>**After Stroke Prediction: No Stroke**</span>", unsafe_allow_html=True)
+                else:
+                    st.write("After Stroke Prediction: Stroke")
+            else:
+                st.error(f'An error occurred during predictions.')
+                if response_nostroke.status_code != 200:
+                    st.write('No Stroke Image Error:', response_nostroke.text)
+                if response_stroke.status_code != 200:
+                    st.write('Stroke Image Error:', response_stroke.text)
+elif uploaded_files and len(uploaded_files) % 2 != 0:
+    st.warning("Please upload the images in pairs for prediction.")
+else:
+    st.warning("Please upload the image files for prediction.")
---
+++ pages/appll.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import requests
+st.markdown("<h1 style='text-align: left;'>Face Paralysis Classification</h1>", unsafe_allow_html=True)
+uploaded_files = st.sidebar.file_uploader(
+    "Upload files for prediction",
+    type=["png", "jpg"], accept_multiple_files=True)
+if uploaded_files:
+    # Sort files alphabetically by file name
+    uploaded_files = sorted(uploaded_files, key=lambda x: x.name)
+    if len(uploaded_files) % 2 == 0:  # Ensure there's an even number of files
+        for i in range(0, len(uploaded_files), 2):
+            file1 = uploaded_files[i]
+            file2 = uploaded_files[i + 1]
+            col1, col2 = st.columns([1, 1])  # Two columns for each pair of images
+            url = 'https://strokediag-xwy6mzfeuq-wn.a.run.app/uploadfile/'
+            # Process and display the first image in the first column
+            with col1:
+                st.image(file1, width=240)
+                files = {'file': (file1.name, file1, file1.type)}
+                response_file1 = requests.post(url, files=files)
+                if response_file1.status_code == 200:
+                    prediction_file1 = response_file1.json().get('prediction')
+                    result_text1 = "No Face Paralysis" if prediction_file1 == 0 else "Face Paralysis Detected"
+                    color1 = "green" if prediction_file1 == 0 else "red"
+                    st.markdown(f"<h4 style='text-align: left; color: {color1};'>{result_text1}</h4>", unsafe_allow_html=True)
+                else:
+                    st.error('Error with First Image:', response_file1.text)
+            # Process and display the second image in the second column
+            with col2:
+                st.image(file2, width=240)
+                files = {'file': (file2.name, file2, file2.type)}
+                response_file2 = requests.post(url, files=files)
+                if response_file2.status_code == 200:
+                    prediction_file2 = response_file2.json().get('prediction')
+                    result_text2 = "No Face Paralysis" if prediction_file2 == 0 else "Face Paralysis Detected"
+                    color2 = "green" if prediction_file2 == 0 else "red"
+                    st.markdown(f"<h4 style='text-align: left; color: {color2};'>{result_text2}</h4>", unsafe_allow_html=True)
+                else:
+                    st.error('Error with Second Image:', response_file2.text)
